4aa204f2122903a2305316e9c79c829aa81b151e
MrT3313 <rturgeon13@gmail.com>
2019-04-06 11:32:39 -0500

    Route & NAvLink PROPERLY added to <App /> from react-router-dom not just react

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component, Route } from 'react';
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import axios from 'axios'
 
 import './App.css';
