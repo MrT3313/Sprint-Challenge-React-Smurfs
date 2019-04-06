9a412f82f1ae366e91fb45f755d604e890aad9c6
MrT3313 <rturgeon13@gmail.com>
2019-04-06 11:54:35 -0500

    onClick added to <NavBar /> buttons. Buttons pass appropriate paths to handler_NavClick method

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -12,13 +12,26 @@ const Styles_nav = styled.div`
 `;
 
 export default class NavBar extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+    
+    handler_NavClick(path) {
+        console.log(`Clicked -- goto ${path}`)
+    }
+
     render() {
-    return (
-        <Styles_nav>
-            <button>Link 1</button>
-            <button>Link 2</button>
-        </Styles_nav>
-    );
+        return (
+            <Styles_nav>
+                <button
+                    onClick={ () => this.handler_NavClick('/') }
+                >Link 1</button>
+                <button
+                    onClick={ () => this.handler_NavClick('/smurf-form') }
+                >Link 2</button>
+            </Styles_nav>
+        );
     }
 }
 
